7f8ddcae0b6b0bcf3a65511bcd0c93ed4abd8eb9
redtechtiger <32938014+redtechtiger@users.noreply.github.com>
2024-04-18 17:03:49 -0400

    Tests: Add tests for basic arithmetic operations

--- src/alg.rs
+++ src/alg.rs
@@ -9,6 +9,7 @@ pub fn decrypt(input: Vec<u8>, key: String) -> Result<String, ()> {
 }
 
 fn quarter_round(a: &mut u32, b: &mut u32, c: &mut u32, d: &mut u32) {
+
     // Stage 1
     *a += *b;
     *d ^= *a;
@@ -29,7 +30,7 @@ fn quarter_round(a: &mut u32, b: &mut u32, c: &mut u32, d: &mut u32) {
     *b ^= *c;
     *b <<= 7;
 
-    todo!("Operations aren't wrapped yet");
+    // todo!("Operations aren't wrapped yet");
 }
 
 #[cfg(test)]
@@ -37,10 +38,18 @@ mod tests {
     use super::*;
 
     #[test]
+    fn arithmetic() {
+        assert_eq!(0x77777777 as u32 + 0x01234567 as u32, 0x789abcde as u32);
+        assert_eq!(0x01020304 as u32 ^ 0x789abcde as u32, 0x7998bfda as u32);
+        assert_eq!(0x7998bfda << 7 as u32, 0xcc5fed3c as u32); // Todo: Figure out why this test is
+                                                               // failing
+    }
+
+    #[test]
     fn quarter_round_1() {
-        let (mut a,mut b,mut c,mut d) = (0x11111111, 0x01020304, 0x9b8d6f43, 0x01234567);
+        let (mut a,mut b,mut c,mut d) = (0x516461b1, 0x2a5f714c, 0x53372767, 0x3d631689);
         quarter_round(&mut a, &mut b, &mut c, &mut d);
-        assert_eq!((a,b,c,d),(0xea2a92f4, 0xcb1cf8ce, 0x4581472e, 0x5881c4bb));
+        assert_eq!((a,b,c,d),(0xbdb886dc, 0xcfacafd2, 0xe46bea80, 0xccc07c79));
     }
 }
 

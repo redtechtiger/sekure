ce07dd43e51bd1fc414556d269a483770660283d
redtechtiger <32938014+redtechtiger@users.noreply.github.com>
2024-04-17 20:15:16 -0400

    Tidy: Change codebase structure

--- src/alg.rs
+++ src/alg.rs
@@ -1,24 +1,7 @@
+pub fn encrypt() {
 
-pub struct Cryptstream {
-    path: String,
 }
 
-impl Cryptstream {
-    pub fn open_cryptstream(path: String, passphrase: String) -> Result<Cryptstream, ()> {
-        
-        todo!(); // Decrypt and read data
-    }
-    pub fn close_cryptstream(&mut self) {
+pub fn decrypt() {
 
-        todo!(); // Close and secure memory
-    }
-    pub fn write(data: String) {
-
-        todo!(); // Write data to memory
-    }
-    pub fn flush() -> Result<(),()> {
-
-        todo!(); // Write encrypted data to disk
-    }
 }
-
---
+++ src/stream.rs
@@ -0,0 +1,23 @@
+pub struct Cryptstream {
+    path: String,
+}
+
+impl Cryptstream {
+    pub fn open_cryptstream(path: String, passphrase: String) -> Result<Cryptstream, ()> {
+        
+        todo!(); // Decrypt and read data
+    }
+    pub fn close_cryptstream(&mut self) {
+
+        todo!(); // Close and secure memory
+    }
+    pub fn write(data: String) {
+
+        todo!(); // Write data to memory
+    }
+    pub fn flush() -> Result<(),()> {
+
+        todo!(); // Write encrypted data to disk
+    }
+}
+

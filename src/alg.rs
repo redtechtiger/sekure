d78a940c19f5770d4ea10ab323f2dc68afecf237
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-04-22 16:28:21 -0400

    Test: Add test case for summing states, and fix index out of bounds

--- src/alg.rs
+++ src/alg.rs
@@ -81,7 +81,7 @@ fn quarter_round(state: &mut [u32; 16], a: usize, b: usize, c: usize, d: usize) 
 fn add_states(state1: [u32; 16], state2: [u32; 16]) -> [u32; 16] {
     // Calculate the sum of both matrices
     let mut sum_state: [u32; 16] = [0; 16];
-    for i in 0..17 {
+    for i in 0..16 {
         sum_state[i] = state1[i].wrapping_add(state2[i]);
     }
     sum_state
@@ -131,8 +131,19 @@ mod tests {
         let state = init_state([0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c], [0x09000000, 0x4a000000, 0x00000000], 1);
         assert_eq!(
             state,
-            [0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, 0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c, 0x00000001,  0x09000000, 0x4a000000,  0x00000000]
+            [0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, 0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c, 0x00000001, 0x09000000, 0x4a000000, 0x00000000]
             );
     }
+
+    #[test]
+    fn add_states_1() {
+        let state1 = [0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, 0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c, 0x00000001, 0x09000000, 0x4a000000, 0x00000000];
+        let state2 = [0x837778abu32, 0xe238d763u32, 0xa67ae21e, 0x5950bb2f, 0xc4f2d0c7, 0xfc62bb2f, 0x8fa018fc, 0x3f5ec7b7, 0x335271c2, 0xf29489f3, 0xeabda8fc, 0x82e46ebd, 0xd19c12b4, 0xb04e16de, 0x9e83d0cb, 0x4e3c50a2];
+        assert_eq!(
+            add_states(state1, state2),
+            [0xe4e7f110, 0x15593bd1, 0x1fdd0f50, 0xc47120a3, 0xc7f4d1c7, 0x0368c033, 0x9aaa2204, 0x4e6cd4c3, 0x466482d2, 0x09aa9f07, 0x05d7c214, 0xa2028bd9, 0xd19c12b5, 0xb94e16de, 0xe883d0cb, 0x4e3c50a2]
+            );
+    }
+
 }
 

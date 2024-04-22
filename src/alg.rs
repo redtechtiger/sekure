38e30fbbda9615674a7301b6a963f3429a0bd216
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-04-22 10:03:24 -0400

    Test: Added another quarter round test

--- src/alg.rs
+++ src/alg.rs
@@ -35,19 +35,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn arithmetic() {
+    fn arithmetic() { // Test which arithmetic functions we are using
         assert_eq!(0x77777777 as u32 + 0x01234567 as u32, 0x789abcde as u32);
         assert_eq!(0x01020304 as u32 ^ 0x789abcde as u32, 0x7998bfda as u32);
         assert_eq!((0x7998bfda as u32).rotate_left(7) as u32, 0xcc5fed3c as u32);
     }
 
     #[test]
-    fn quarter_round_1() {
+    fn quarter_round_1() { // Test that the quarter round produces correct results
         let (mut a, mut b, mut c, mut d) = (0x516461b1, 0x2a5f714c, 0x53372767, 0x3d631689);
         quarter_round(&mut a, &mut b, &mut c, &mut d);
         assert_eq!(
             (a, b, c, d),
             (0xbdb886dc, 0xcfacafd2, 0xe46bea80, 0xccc07c79)
+        );
+    }
+
+    #[test]
+    fn quarter_round_2() { // Test that the quarter round produces correct results
+        let (mut a, mut b, mut c, mut d) = (0x11111111, 0x01020304, 0x9b8d6f43, 0x01234567);
+        quarter_round(&mut a, &mut  b, &mut c, &mut d);
+        assert_eq!(
+            (a, b, c, d),
+            (0xea2a92f4, 0xcb1cf8ce, 0x4581472e, 0x5881c4bb)
         );
     }
 }

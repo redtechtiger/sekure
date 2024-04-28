dedd77e08aff0d02adbed049b2cc0815402d83a9
RedTechTiger <32938014+redtechtiger@users.noreply.github.com>
2024-04-27 22:35:26 -0400

    Polyauth: Work on authentication and modify function names

--- src/polyauth.rs
+++ src/polyauth.rs
@@ -1,4 +1,18 @@
-fn poly_clamp(r: &mut [u8; 16]) -> () {
+pub fn generate(msg: &[u8], key: [u8;32]) -> Vec<u8> {
+    let r = clamp(&mut key[0..15].try_into().unwrap()); // TODO: Figure out if we can get rid of
+                                                        // the ugly .try_into().unwrap()
+    let s = &key[16..31];
+    let P = 0x3fffffffffffffffffffffffffffffffbu128; // TODO: Fix this garbage
+    let mut acc = 0;
+
+    for i in 0..msg.len().div_ceil(16) { // Iterate over every 16 byte block
+        // Do stuff (see reference RFC 7539, 2.5)
+    }
+
+    todo!();
+}
+
+fn clamp(r: &mut [u8; 16]) -> () {
     // Clear top 4 bits
     r[3] &= 0b00001111;
     r[7] &= 0b00001111;

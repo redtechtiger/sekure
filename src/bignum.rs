2cf975da74d9286e27b12369be5b3212c0de5dfd
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-04-30 10:50:10 -0400

    Bignum: Add scaffolding for functions and type

--- src/bignum.rs
+++ src/bignum.rs
@@ -1,1 +1,11 @@
+pub struct BigU288([u8;36]); // 288 bit unsigned integer (8x36)
+
+impl Add for BigU288 {
+    fn add(&self, other: BigU288) -> BigU288 {
+        
+    }
+    fn add_msb(&mut self) {
+        
+    }
+}
 

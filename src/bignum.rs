24e035f99f48028c9e0f38466aa0b8649c591985
RedTechTiger <32938014+redtechtiger@users.noreply.github.com>
2024-05-05 17:49:48 -0400

    Bignum: Tidy: Run rustfmt

--- src/bignum.rs
+++ src/bignum.rs
@@ -1,19 +1,18 @@
 use std::ops::Add;
 
-
 #[derive(Debug)]
 pub struct BigU288([u8; 36]); // 288 bit unsigned integer (8x36)
-
 
 impl Add for BigU288 {
     type Output = BigU288;
     fn add(self, other: Self) -> Self::Output {
         let mut output = BigU288::new();
-        for (index, byte) in self.0.iter().enumerate() { // LSB first
+        for (index, byte) in self.0.iter().enumerate() {
+            // LSB first
             output.0[index] += byte.wrapping_add(other.0[index]);
             let carry = (*byte as u16 + other.0[index] as u16) - output.0[index] as u16;
-            output.0[std::cmp::min(index+1, output.0.len()-1)] = carry as u8;
-        };
+            output.0[std::cmp::min(index + 1, output.0.len() - 1)] = carry as u8;
+        }
         output
     }
 }
@@ -30,8 +29,8 @@ impl BigU288 {
         // TODO: Important! Attempt to solve this in constant time
         let mut i: bool = false; // Flag to see if we've hit the msb yet
         for (index, byte) in self.0.iter().rev().enumerate() { // Enumerate backwards (msb first)
-            // let bit_1 = byte & 0b1000_0000; // Shift 
-            // let bit_1 = byte & 0b0000_0001; // Shift 
+             // let bit_1 = byte & 0b1000_0000; // Shift
+             // let bit_1 = byte & 0b0000_0001; // Shift
         }
         todo!("msb isn't implemented yet");
     }
@@ -45,13 +44,15 @@ impl BigU288 {
     pub fn from_hex(input: &str) -> BigU288 {
         let mut big_u288 = BigU288::new();
         // Iterate over the string backwards (we want little endian)
-        for (index, char) in input.chars().rev().enumerate() { // TODO: Make this constant time!!!
-            let hex_digit = u8::from_str_radix(&char.to_string(), 16).expect("invalid character found");
-            big_u288.0[index/2] += hex_digit << 4*(index%2);
-        };
+        for (index, char) in input.chars().rev().enumerate() {
+            // TODO: Make this constant time!!!
+            let hex_digit =
+                u8::from_str_radix(&char.to_string(), 16).expect("invalid character found");
+            big_u288.0[index / 2] += hex_digit << 4 * (index % 2);
+        }
         big_u288
     }
-    pub fn get_bytes(&self) -> [u8;36] {
+    pub fn get_bytes(&self) -> [u8; 36] {
         self.0
     }
     pub fn new() -> BigU288 {
@@ -66,7 +67,7 @@ mod tests {
     // #[test]
     // fn add_msb_1() {
     // }
-    
+
     // #[test]
     // fn from_hex_1() {
     //     let big_u288 = BigU288::from_hex("3fffffffffffffffffffffffffffffffb");
@@ -79,7 +80,10 @@ mod tests {
     fn from_hex_2() {
         assert_eq!(
             BigU288::from_hex("f").get_bytes(),
-            [15u8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            [
+                15u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0
+            ]
         );
     }
 
@@ -87,7 +91,10 @@ mod tests {
     fn from_hex_3() {
         assert_eq!(
             BigU288::from_hex("ff").get_bytes(),
-            [255u8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            [
+                255u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            ]
         );
     }
 
@@ -95,7 +102,10 @@ mod tests {
     fn from_hex_4() {
         assert_eq!(
             BigU288::from_hex("1fff").get_bytes(),
-            [255u8,31u8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            [
+                255u8, 31u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            ]
         );
     }
 
@@ -115,4 +125,3 @@ mod tests {
         );
     }
 }
-

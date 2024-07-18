fc682e35e4cd74a67880f8dc5ade9086cefb58b8
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-18 12:49:05 +0200

    Dep: Add sha2 as a dependency

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -1,7 +1,9 @@
+// A is the amount of bytes the cryptographic key shall contain
 pub fn derive_cryptographic_key<const A: usize>(
     password: &str,
     salt: [u8; 128],
     iteration_count: usize,
 ) -> [u8; A] {
+    
     todo!();
 }

f1f6e7e6e53695cc0855f0624e87f57ea27b9de0
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-18 12:53:30 +0200

    PBKDF2: Add bounds checking for length of master key generation

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -4,6 +4,9 @@ pub fn derive_cryptographic_key<const A: usize>(
     salt: [u8; 128],
     iteration_count: usize,
 ) -> [u8; A] {
+    if A > (2usize.pow(32)-1) * 256 { // SHA256 is used
+        panic!("master key length exceeds maximum allowed by sha256");
+    }
     
     todo!();
 }

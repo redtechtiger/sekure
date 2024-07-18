ca1f09e05b1ec736d7a1a80d63175fd788bf13b7
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-18 12:27:27 +0200

    PBKDF2: Change function signature to accept &str instead of String and run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod pbkdf2;
 mod chacha20;
 mod cryptstream;
+mod pbkdf2;
 mod poly1305;
--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -1,3 +1,7 @@
-pub fn derive_cryptographic_key<const A: usize>(password: String, salt: [u8; 128], iteration_count: usize) -> [u8; A] {
+pub fn derive_cryptographic_key<const A: usize>(
+    password: &str,
+    salt: [u8; 128],
+    iteration_count: usize,
+) -> [u8; A] {
     todo!();
 }

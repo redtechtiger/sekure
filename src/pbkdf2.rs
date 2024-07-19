4f02fd23ba13d47d7beaebd672367fe68baffda1
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-19 23:31:16 +0200

    PBKDF2: Change uconst![] to pconst![] in const division for consistency

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -1,5 +1,5 @@
 use typenum::{PartialDiv, Integer, P256};
-use typenum_consts::uconst;
+use typenum_consts::pconst;
 type DIGEST_SIZE = P256; // SHA256 is used
 
 // KEYLEN is the amount of bytes the cryptographic key shall contain
@@ -12,7 +12,7 @@ pub fn derive_cryptographic_key<const KEYLEN: usize>(
         panic!("master key length exceeds maximum allowed by sha256");
     }
 
-    type len = PartialDiv<uconst![KEYLEN], DIGEST_SIZE>;
+    type len = PartialDiv<pconst![KEYLEN], DIGEST_SIZE>;
     const len: usize = <len as Integer>::to_i64() as usize;
 
     let r = KEYLEN - (len - 1) * 256;

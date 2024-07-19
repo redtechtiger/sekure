31d25bade0cc36d0851b00da55bc9c259ded1182
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-19 22:54:06 +0200

    PBKDF2: Extract digest size to const usize and begin work on algorithm (stupid const expressions...)

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -1,11 +1,22 @@
-// A is the amount of bytes the cryptographic key shall contain
-pub fn derive_cryptographic_key<const A: usize>(
+const DIGEST_SIZE: usize = 256; // SHA256 is used
+
+// KEYLEN is the amount of bytes the cryptographic key shall contain
+pub fn derive_cryptographic_key<const KEYLEN: usize>(
     password: &str,
     salt: [u8; 128],
     iteration_count: usize,
-) -> [u8; A] {
-    if A > (2usize.pow(32)-1) * 256 { // SHA256 is used
+) -> [u8; KEYLEN] {
+    if KEYLEN > (2usize.pow(32)-1) * DIGEST_SIZE {
         panic!("master key length exceeds maximum allowed by sha256");
+    }
+
+    const len: usize = f64::ceil(KEYLEN as f64 / DIGEST_SIZE as f64) as usize;
+    let r = KEYLEN - (len - 1) * 256;
+
+    let t = [0u8; len];
+
+    for i in 1..=len {
+        
     }
     
     todo!();

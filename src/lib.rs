721d2eddaa2925f76f95c80320b25c87847e22da
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-20 18:17:29 +0200

    PBKDF2: Revert unnecessary external crates and pray that the constant expressions decide to work

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(generic_const_exprs)]
 pub mod chacha20;
 pub mod cryptstream;
 pub mod pbkdf2;
--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -1,6 +1,4 @@
-use typenum::{PartialDiv, Integer, P256};
-use typenum_consts::pconst;
-type DIGEST_SIZE = P256; // SHA256 is used
+const DIGEST_SIZE: usize = 256; // SHA256 is used
 
 // KEYLEN is the amount of bytes the cryptographic key shall contain
 pub fn derive_cryptographic_key<const KEYLEN: usize>(
@@ -8,12 +6,11 @@ pub fn derive_cryptographic_key<const KEYLEN: usize>(
     salt: [u8; 128],
     iteration_count: usize,
 ) -> [u8; KEYLEN] {
-    if KEYLEN > (2usize.pow(32)-1) * <DIGEST_SIZE as Integer>::to_isize() as usize {
+    if KEYLEN > (2usize.pow(32)-1) * DIGEST_SIZE {
         panic!("master key length exceeds maximum allowed by sha256");
     }
 
-    type len = PartialDiv<pconst![KEYLEN], DIGEST_SIZE>;
-    const len: usize = <len as Integer>::to_i64() as usize;
+    const len: usize = (KEYLEN / DIGEST_SIZE) + (KEYLEN % DIGEST_SIZE != 0) ;
 
     let r = KEYLEN - (len - 1) * 256;
     let t = [0u8; len];

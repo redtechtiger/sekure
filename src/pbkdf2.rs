7780f4fe5a166629b50f46c6b9ddcd64f6bf9019
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-22 18:21:59 +0200

    PBKDF2: Run rustfmt

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -6,18 +6,19 @@ pub fn derive_cryptographic_key<const KEYLEN: usize>(
     password: &str,
     salt: [u8; 128],
     iteration_count: usize,
-) -> [u8; KEYLEN] where [(); derive_len(KEYLEN)]: {
-    if KEYLEN > (2usize.pow(32)-1) * DIGEST_SIZE {
+) -> [u8; KEYLEN]
+where
+    [(); derive_len(KEYLEN)]:,
+{
+    if KEYLEN > (2usize.pow(32) - 1) * DIGEST_SIZE {
         panic!("master key length exceeds maximum allowed by sha256");
     }
 
     let r = KEYLEN - (derive_len(KEYLEN) - 1) * 256;
     let t = [0u8; derive_len(KEYLEN)];
 
-    for i in 1..=derive_len(KEYLEN) {
-        
-    }
-    
+    for i in 1..=derive_len(KEYLEN) {}
+
     todo!();
 }
 

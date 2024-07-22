6b63dc9608697db955f29a73ee361d8225557d3f
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-22 18:02:24 +0200

    PBKDF2: Extract len calculation logic into separate function and fix compilation issues

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -2,22 +2,25 @@ const DIGEST_SIZE: usize = 256; // SHA256 is used
 
 // KEYLEN is the amount of bytes the cryptographic key shall contain
 pub fn derive_cryptographic_key<const KEYLEN: usize>(
+    keylen: usize,
     password: &str,
     salt: [u8; 128],
     iteration_count: usize,
-) -> [u8; KEYLEN] {
+) -> [u8; KEYLEN] where [(); derive_len(KEYLEN)]: {
     if KEYLEN > (2usize.pow(32)-1) * DIGEST_SIZE {
         panic!("master key length exceeds maximum allowed by sha256");
     }
 
-    const len: usize = (KEYLEN / DIGEST_SIZE) + (KEYLEN % DIGEST_SIZE != 0) ;
+    let r = KEYLEN - (derive_len(KEYLEN) - 1) * 256;
+    let t = [0u8; derive_len(KEYLEN)];
 
-    let r = KEYLEN - (len - 1) * 256;
-    let t = [0u8; len];
-
-    for i in 1..=len {
+    for i in 1..=derive_len(KEYLEN) {
         
     }
     
     todo!();
+}
+
+const fn derive_len(keylen: usize) -> usize {
+    (keylen / DIGEST_SIZE) + (keylen % DIGEST_SIZE != 0) as usize
 }

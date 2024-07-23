12beadfdce53c183ab3893362e3a8fc513fec8a6
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-23 23:24:11 +0200

    PBKDF2: Initial groundwork done

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -2,7 +2,7 @@ use sha2::Sha256;
 use hmac::{Hmac, Mac};
 use sha2::digest::typenum::U256;
 const DIGEST_SIZE: usize = 256; // SHA256 is used
-type DigestType = U256;
+type DigestType = [u8; DIGEST_SIZE / 8];
 
 // KEYLEN is the amount of bytes the cryptographic key shall contain
 pub fn derive_cryptographic_key<const KEYLEN: usize, const ITERATION_COUNT: usize>(
@@ -20,7 +20,7 @@ where
 
     // Derive the actual cryptographic key
     let octets_in_last_block = KEYLEN - (derive_num_blocks(KEYLEN) - 1) * DIGEST_SIZE; // Needed?
-    let mut t: [DigestType; KEYLEN] = [DigestType::new(); KEYLEN];
+    let mut t: [DigestType; KEYLEN] = [[0u8; DIGEST_SIZE / 8]; KEYLEN];
     for i in 1..=derive_num_blocks(KEYLEN) {
         t[i] = f::<ITERATION_COUNT>(password, salt, i);
     }
@@ -35,7 +35,16 @@ where
 }
 
 fn f<const ITERATION_COUNT: usize>(password: &str, salt: [u8; 128], index: usize) -> DigestType {
-    
+    let mut u: [DigestType; ITERATION_COUNT] = [[0u8; DIGEST_SIZE / 8]; ITERATION_COUNT];
+    // Initial hash will be salt with the index concatenated onto it
+    let mut initial_concat = [0u8; 128+4]; // Length of salt plus index (converted to 4 bytes)
+    initial_concat[0..128].copy_from_slice(&salt);
+    initial_concat[128..].copy_from_slice(&(index as i32).to_be_bytes());
+
+    u[0] = hmac_sha256(password, &initial_concat);
+    for i in 1..ITERATION_COUNT {
+        
+    }
     todo!();
 }
 
@@ -43,7 +52,7 @@ const fn derive_num_blocks(keylen: usize) -> usize {
     (keylen / DIGEST_SIZE) + (keylen % DIGEST_SIZE != 0) as usize
 }
 
-fn hmac_sha256(password: &str, input: &[u8]) -> [u8; 256/8] {
+fn hmac_sha256(password: &str, input: &[u8]) -> [u8; DIGEST_SIZE/8] {
     let mut mac = Hmac::<Sha256>::new_from_slice(password.as_bytes()).expect("hmac key initialization failed");
     mac.update(input);
     let result = mac.finalize();

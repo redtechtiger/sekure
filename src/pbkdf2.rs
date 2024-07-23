2abd06f32226ae9d176e90b60f838a84ad82f5cb
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-07-23 20:42:21 +0200

    PBKDF2: Refactor code to match the layout as described in RFC 2898

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -1,4 +1,7 @@
+use sha2::Sha256;
+use sha2::digest::typenum::U256;
 const DIGEST_SIZE: usize = 256; // SHA256 is used
+type DigestType = U256;
 
 // KEYLEN is the amount of bytes the cryptographic key shall contain
 pub fn derive_cryptographic_key<const KEYLEN: usize, const ITERATION_COUNT: usize>(
@@ -6,26 +9,28 @@ pub fn derive_cryptographic_key<const KEYLEN: usize, const ITERATION_COUNT: usiz
     salt: [u8; 128],
 ) -> [u8; KEYLEN]
 where
-    [(); derive_len(KEYLEN)]:,
-    [(); derive_len(KEYLEN)+1]:,
+    [(); derive_num_blocks(KEYLEN)]:,
     [(); ITERATION_COUNT+1]:,
 {
     if KEYLEN > (2usize.pow(32) - 1) * DIGEST_SIZE {
-        panic!("master key length exceeds maximum allowed by sha256");
+        panic!("derived key too long");
     }
 
-    let r = KEYLEN - (derive_len(KEYLEN) - 1) * DIGEST_SIZE;
-    let t = [0u8; derive_len(KEYLEN)+1];
-    let u = [0u8; ITERATION_COUNT+1];
-
-    for i in 1..=derive_len(KEYLEN) {
+    let octets_in_last_block = KEYLEN - (derive_num_blocks(KEYLEN) - 1) * DIGEST_SIZE; // Needed?
+    let t: [DigestType; KEYLEN];
+    for i in 1..=derive_num_blocks(KEYLEN) {
         
     }
 
     todo!();
 }
 
-const fn derive_len(keylen: usize) -> usize {
+fn f<const ITERATION_COUNT: usize>(password: &str, salt: [u8; 128], index: usize) -> DigestType {
+
+    todo!();
+}
+
+const fn derive_num_blocks(keylen: usize) -> usize {
     (keylen / DIGEST_SIZE) + (keylen % DIGEST_SIZE != 0) as usize
 }
 
